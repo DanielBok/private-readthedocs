5deb82d65dfb16024c38902e528065d28f28a2b8
Daniel Bok <daniel.bok@outlook.com>
2020-04-08 23:09:17 +0800

    Change account document to a one-to-many relationship

--- services/database/migrations/01_accounts.up.sql
+++ services/database/migrations/01_accounts.up.sql
@@ -10,11 +10,6 @@ CREATE TABLE document
 (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(255) UNIQUE CHECK ( length(name) >= 1 ) NOT NULL,
-    last_update TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE account_document
-(
-    account_id  SERIAL REFERENCES account (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    document_id SERIAL REFERENCES document (id) ON UPDATE CASCADE ON DELETE CASCADE
+    last_update TIMESTAMP DEFAULT NOW(),
+    account_id  INT REFERENCES account (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
